890fcbc83ee9640ca6d3f35b995aad2b36fa919d
Peter Tillemans <pti@snamellit.com>
2019-09-09 16:06:10 +0200

    add application authoentication flow

--- src/Run.hs
+++ src/Run.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+
 module Run (run) where
 
 import Import
@@ -8,13 +12,45 @@ import Control.Lens ((.~), (<&>))
 
 import Network.Google
 import Network.Google.AppsCalendar
+import Network.Google.Auth
 
+import Data.Proxy     (Proxy (..))
+import Data.Text      as T
+import Data.Text.IO   as T
+import System.Info    (os)
+import System.Process (rawSystem)
+import GHC.TypeLits   (Symbol)
+
+
+redirectPrompt :: AllowScopes (s :: [Symbol]) => OAuthClient -> proxy s -> IO (OAuthCode s)
+redirectPrompt c p = do
+  let url = formURL c p
+  T.putStrLn $ "Opening URL " `T.append` url
+  _ <- case os of
+    "darwin" -> rawSystem "open"     [unpack url]
+    "linux"  -> rawSystem "xdg-open" [unpack url]
+    "mingw32" -> rawSystem "explorer" [unpack url]
+    _        -> T.putStrLn "Unsupported OS" >> exitFailure
+  T.putStrLn "Please input the authorisation code: "
+  OAuthCode <$> T.getLine
+
+
+clientId :: ClientId
+clientId = ClientId "911970396904-hhbksdk352ee1isb7fp4tg9h6qdo8kn9.apps.googleusercontent.com"
+clientSecret :: GSecret
+clientSecret = GSecret "VwpMVKPBCZkLRTauGu55D-cZ"
+oauthClient = OAuthClient clientId clientSecret
 
 
 getCalendars :: IO CalendarList
 getCalendars = do
+  oauthCode <- redirectPrompt oauthClient calendarScope
   lgr <- newLogger Debug stdout
-  env <- newEnv <&> (envLogger .~ lgr) . (envScopes .~ calendarScope)
+  mgr <- newManager tlsManagerSettings
+  env <- newEnvWith 
+          (installedApplication oauthClient oauthCode) 
+          lgr mgr
+          
   runResourceT . runGoogle env $ send calendarListList
 
 run :: RIO App ()
